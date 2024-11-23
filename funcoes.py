b9c2a885e1683a27d3808115a148ee4d36cf701d
igorfeedias <162120855+igorfeedias@users.noreply.github.com>
2024-11-23 17:19:21 +0000

    exericio

---
+++ funcoes.py
@@ -0,0 +1,20 @@
+def somar (a,b):
+    return a + b
+
+def f(x):
+    return x*x +1
+y= f(3)
+print ("O valor de y é", y)
+
+#Funcao 1
+def f(x):
+    return x*x+2*x+5
+
+def g(x):
+    return x+4
+y= f(5)
+z= g(4)
+x= f(g(4))
+print ("o valor de f é", y)
+print ("o valor de g é", z)
+print ("o valor de x é", x)
