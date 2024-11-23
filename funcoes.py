8fdfdccab212e293cce61931064375fb068ab049
igorfeedias <162120855+igorfeedias@users.noreply.github.com>
2024-11-23 17:25:12 +0000

    Teste

--- funcoes.py
+++ funcoes.py
@@ -14,7 +14,8 @@ def g(x):
     return x+4
 y= f(5)
 z= g(4)
-x= f(g(4))
+k= int(input("Informe o valor de k:"))
+x= f(g(k))
 print ("o valor de f é", y)
 print ("o valor de g é", z)
 print ("o valor de x é", x)
