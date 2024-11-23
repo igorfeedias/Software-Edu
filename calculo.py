4febf3d716e7c25852c4b24b577bf2c4ff868658
igorfeedias <162120855+igorfeedias@users.noreply.github.com>
2024-11-23 18:24:54 +0000

    desconto

--- calculo.py
+++ calculo.py
@@ -2,7 +2,16 @@
 def calcular_compra(valor_unitario, numero_itens):
     return valor_unitario * numero_itens
 
+def aplicar_desconto(total_compra, desconto):
+    desconto = total_compra *(desconto/100)
+    return total_compra - desconto
+
 valor_unitario= int(input("Digite o valor unitário do produto:"))
 quantidade_itens= int(input("Digite a quantidade do produto:"))
+
 valor_total= calcular_compra(valor_unitario,quantidade_itens)
-print(f"O valor do Total da Compra é R$ {valor_total:.2f}")
+d= int(input("Informe o desconto a ser aplicado:"))
+
+total_com_desconto= aplicar_desconto (valor_total,d)
+
+print(f"O valor do Total da Compra é R$ {total_com_desconto:.2f}")
