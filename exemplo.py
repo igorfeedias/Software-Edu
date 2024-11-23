e8012ce83a2a640faf72e5fcb32e6cb63e3dce44
igorfeedias <162120855+igorfeedias@users.noreply.github.com>
2024-11-23 16:59:47 +0000

    exe

--- exemplo.py
+++ exemplo.py
@@ -1,5 +1,9 @@
 def somar(a, b):
     return a + b
 
-X= 2 + 3
-print("O valor da soma é", X)
+x= somar (5,9)
+y= somar (95,32)
+z= somar (257, 837)
+print("O valor da soma é", x)
+print("O valor da soma é", y)
+print("O Valor da soma é", z)
