67a48b0c4cc1905f38ff283239db14a41da74757
igorfeedias <162120855+igorfeedias@users.noreply.github.com>
2024-11-23 17:53:07 +0000

    Exe

--- calculo.py
+++ calculo.py
@@ -5,4 +5,4 @@ def calcular_compra(valor_unitario, numero_itens):
 v_u= float(input("Digite o valor unitário do produto:"))
 q_i= int(input("Digite a quantidade do produto:"))
 valor_total= calcular_compra(v_u,q_i)
-print(f"O valor do Total da Compra è R$ {valor_total:.2f}")
+print(f"O valor do Total da Compra é R$ {valor_total:.2f}")
